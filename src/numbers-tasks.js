95f9bef788b3c85e6f32fa2104b798595eb246d9
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-12-04 17:30:37 +0300

    feat: copy functions implementations from the my core-js-101 stage 0 task decisions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -90,8 +90,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.hypot(x2 - x1, y2 - y1);
 }
 
 /**
@@ -106,8 +106,9 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  // a * x + b = 0 ===> x = -b / a;
+  return -b / a;
 }
 
 /**
@@ -127,8 +128,16 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  // formula:
+  // cos Angl = (x1 * x2 + y1 * y2) / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))
+  // Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2)
+  // is e.q. multiplication of vectors X and Y lengths
+  const multXY = x1 * x2 + y1 * y2;
+  const multLengthXLengthY = Math.hypot(x1, y1) * Math.hypot(x2, y2);
+  const cosOfAngle = multXY / multLengthXLengthY;
+  const angleBetweenVectorsDeg = Math.acos(cosOfAngle);
+  return angleBetweenVectorsDeg;
 }
 
 /**
@@ -144,8 +153,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const digitCount = String(value).length;
+  return value % 10 ** (digitCount > 1 ? digitCount - 1 : digitCount);
 }
 
 /**
@@ -159,8 +169,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -176,8 +186,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -197,8 +207,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -218,8 +228,36 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  function eratosthenesSieve(maxNumber) {
+    const limitOfSearch = maxNumber;
+    const iterableArrOfNumbers = new Array(limitOfSearch)
+      .fill(true)
+      .map((elem, index) => (index === 0 ? elem === false : elem))
+      .map((elem, index) => (index === 1 ? elem === false : elem));
+    const primeNumbersArray = [1];
+
+    for (let i = 2; i < iterableArrOfNumbers.length; i += 1) {
+      if (iterableArrOfNumbers[i] === true) {
+        primeNumbersArray.push(i);
+
+        for (
+          let j = 1;
+          j <= Math.round(iterableArrOfNumbers.length / 2);
+          j += 1
+        ) {
+          if (iterableArrOfNumbers[i * j] <= iterableArrOfNumbers.length) {
+            iterableArrOfNumbers[i * j] = false;
+          }
+        }
+      }
+    }
+    return primeNumbersArray;
+  }
+
+  const primeNumbersArray = eratosthenesSieve(10000);
+
+  return primeNumbersArray.includes(n);
 }
 
 /**
@@ -237,8 +275,16 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isFinite(+value)) {
+    return +value;
+  }
+
+  if (value instanceof Number) {
+    return value;
+  }
+
+  return def;
 }
 
 /**
