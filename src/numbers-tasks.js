d08133fbe4f11b21c6e8519f956ee23e9370e2e4
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-12-05 23:39:51 +0300

    feat: implement getRandomInteger function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -707,8 +707,11 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    throw new Error('Not an ineger numbers were given');
+  }
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 /**
